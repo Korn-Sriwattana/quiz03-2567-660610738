ff5ea74a13a7110b9e22fbb42e7c5bd42daf87ec
Korn Sriwattana <korn_s@cmu.ac.th>
2024-09-26 15:43:30 +0700

    rty

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { Database } from "@lib/DB";
 
 
-export const GET = async (request: NextRequest) => {
+export const GET = async () => {
   readDB();
   const payload = checkToken();
   const { roomId } = <Payload>payload;
@@ -41,8 +41,7 @@ export const GET = async (request: NextRequest) => {
   }
 
 export const POST = async (request: NextRequest) => {
-  const payload = checkToken();
-  readDB();
+  readDB();s
   const body = await request.json();
   const { roomId } = body;
 
